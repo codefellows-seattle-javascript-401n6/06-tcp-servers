c055e6b33f8aa48c0e68ab991478bc5843dce570
daniel <dpillay03@gmail.com>
2018-02-20 18:29:36 -0800

    connected to telnet with a client id

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -2,11 +2,18 @@
 
 const net = require('net');
 const EE = require('events');
-// const Client = require('../lib/client,js');
+const Client = require(`${__dirname}/lib/client.js`);
 const PORT = process.env.PORT || 3000;
 const server = net.createServer();
 const ee = new EE();
 
+const pool = [];
+
+server.on('connection', (socket) => {
+    let client = new Client(socket);
+    pool.push(client);
+    console.log(client.nickname);
+});
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}!`);

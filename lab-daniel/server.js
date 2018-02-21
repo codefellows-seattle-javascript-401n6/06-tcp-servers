c3f9b5acd68ac4e4621b8678871e3171cec18ee8
daniel <dpillay03@gmail.com>
2018-02-20 18:47:24 -0800

    connection portion complete. moving on to nickname

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -9,12 +9,26 @@ const ee = new EE();
 
 const pool = [];
 
+//Creates a client. Issues it an ID.
 server.on('connection', (socket) => {
     let client = new Client(socket);
     pool.push(client);
     console.log(client.nickname);
+    socket.on('data', (data) => {
+        const textInput = data.toString().split(' ').shift().trim();
+        console.log(textInput);
+        if(command.startsWith('@')){
+            ee.emit(command, client, data.toString().split().splice(1)).join(' ');
+            console.log('it works!');
+            return;
+        }
+
+        ee.emit('default');
+    });
 });
 
+
+//Tells you which port you are on.
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}!`);
 });

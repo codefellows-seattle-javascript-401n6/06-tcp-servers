2632ea30498397d50cd3e89ed2e70dd6af586ff0
daniel <dpillay03@gmail.com>
2018-02-20 17:16:51 -0800

    required dependencies and modules on my server. Port is running!

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const net = require('net');
+const EE = require('events');
+// const Client = require('../lib/client,js');
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+const ee = new EE();
+
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}!`);
+});

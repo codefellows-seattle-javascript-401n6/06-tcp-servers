9f560137ac382a607cf6786af3190136cf04727d
rjtj2007 <teacherman1@gmail.com>
2018-02-20 19:10:34 -0800

    created client.js file, server listening on PORT 3000 in server.js

--- lab-ryan/server.js
+++ lab-ryan/server.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = ('./lib/client.js');
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});

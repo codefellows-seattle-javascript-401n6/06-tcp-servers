84312062fc3ca79b9c4a800101c9b96a8a29e57e
amgranad <ambergkim@gmail.com>
2018-02-20 21:49:28 -0800

    mvp plus help command

--- lab-amber/models/client.js
+++ lab-amber/models/client.js
@@ -2,7 +2,7 @@
 
 const uuidv4 = require('uuid/v4');
 
-const Client = module.exports = socket => {
+const Client = module.exports = function(socket) {
   this.socket = socket;
   this.nickname = `user_${Math.random()}`;
   this.id = uuidv4();

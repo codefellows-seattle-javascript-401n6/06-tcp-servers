460383a7eb1c1160443008c0054a243c810c9c60
daniel <dpillay03@gmail.com>
2018-02-23 14:48:33 -0800

    lab is finished. I added a @quit function along with an @all function.

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -9,10 +9,34 @@ const ee = new EE();
 
 const pool = [];
 
-
 //Alerts user that they need to use @ symbol.
 ee.on('default',(client) => {
-    client.socket.write('not a command - use and @ symbol - - >');
+    client.socket.write('not a command - use and @ symbol\n');
+});
+
+//Allows user to exit the chat.
+ee.on('@quit', function(client){
+    client.socket.end('Goodbeye');
+    var index = pool.indexOf(client)
+    if (index > -1) {
+        pool.splice(index, 1);
+    }
+})
+
+//Sends message to all the users.
+ee.on('@all', function(client, string){
+    pool.forEach(user => {
+        if(user.nicname !== client.nickname){
+            user.socket.write(`${client.nickname}, ${string}`)
+        }
+    });
+});
+
+//Allows user to create nickname
+ee.on('@nickname', (client, string) => {
+    let nickname = string.toString().split(' ').shift().trim();
+    client.nickname = nickname;
+    client.socket.write(`Your nickname is now: ${nickname}`);
 });
 
 
@@ -23,7 +47,7 @@ ee.on('@dm', (client, string) => {
 
     pool.forEach( c => {
         if(c.nickname === nickname){
-            c.socket.write(`${client.nickname}: ${message}`);
+            c.socket.write(`${client.nickname}: ${message}\n`);
         }
     });
 });
@@ -33,6 +57,17 @@ ee.on('@list', (client, string) => {
     pool.forEach(users =>{
         users.socket.write(`User id: ${client.nickname}\n`);
     });
+});
+
+//Gives user a list of the commands.
+ee.on('@help', (client) => {
+   client.socket.write('How to use this chat:\n');
+   client.socket.write('@help: list the commands\n');
+   client.socket.write('@nickname: change your chat name\n');
+   client.socket.write('@list: shows all users\n');
+   client.socket.write('@all: message all users\n');
+   client.socket.write('@dm: message specific users only\n');
+   client.socket.write('@quit: exit the chatroom\n');
 });
 
 //Creates a user. Issues them an ID.
@@ -52,14 +87,6 @@ server.on('connection', (socket) => {
         ee.emit('default', client);
     });
 });
-
-//Allows user to create nickname
-ee.on('@nickname', (client, string) => {
-    let nickname = string.toString().split(' ').shift().trim();
-    client.nickname = nickname;
-    client.socket.write(`Your nickname is now: ${nickname}`);
-});
-
 
 //Tells you which port you are on.
 server.listen(PORT, () => {

4c827803fe6720a174a39695298600329ad0cb7e
rjtj2007 <teacherman1@gmail.com>
2018-02-24 16:02:45 -0800

    functioning properly

--- lab-ryan/lib/client.js
+++ lab-ryan/lib/client.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const uuidv4 = require ('uuid/v4');
+const uuidv4 = require('uuid/v4');
 const Client = module.exports = function(socket) {
     this.socket = socket; //information about the connection
     this.nickname = `user_${Math.random()}`; //guest username=unique identifer
     this.id = uuidv4();
-};
+}
--- lab-ryan/server.js
+++ lab-ryan/server.js
@@ -1,12 +1,8 @@
-import { Socket } from 'dgram';
-
 'use strict';
-
-//telnet 192.168.0.8 3000 not working in terminal
 
 const net = require('net');
 const EE = require('events');
-const Client = ('./lib/client.js');
+const Client = require('./lib/client.js');
 const PORT = process.env.PORT || 3000;
 const server = net.createServer();
 const ee = new EE();
@@ -15,20 +11,37 @@ const clientPool = [];
 
 ee.on('default', function(client){
     client.socket.write('not a command - please use @ symbol\n');
+    console.error('client error');
 });
 
-ee.on('@all', function(client, string){
+ee.on('@all', function(client, message) {
     clientPool.forEach( c => {
-        c.socket.write(`${client.nickname}; ${string}`);
-    })
+        c.socket.write(`${client.nickname}: ${message}\n`);
+    });
 });
 
-ee.prependListener('@dm', function(client, string){
+ee.on('@quit', client => {
+    let clientIndex = clientPool.indexOf(client);
+    clientPool.splice(clientIndex, 1);
+    client.socket.end(`See you next time!\n`);
+});
+
+ee.on('@list', client => {
+    client.socket.write(`current user:\n`);
+    clientPool.forEach(user => {
+        client.socket.write(`${user.nickname}\n`);
+    });
+});
+
+ee.on('@dm', function(client, string){
     let nickname = string.split(' ').shift().trim();
     let message = string.split(' ').splice(1).join(' ').trim();
 
     clientPool.forEach( c => {
-    })
+        if(c.nickname === nickname){
+            c.socket.write(`${client.nickname}: ${message}\n`);
+        }
+    });
 });
 
 ee.on('@nickname', function(client, string){
@@ -42,20 +55,17 @@ server.on('connection', function(socket) {
     clientPool.push(client);
 
     socket.on('data', function(data) {
-        const command = data.toString().split(' ').shift().trim();  // strip off /r and /n
-        
+        const command = data.toString().split(' ').shift().trim();
+
         if(command.startsWith('@')) {
             ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
-            console.log('success');
+            console.log('user ', client.nickname, ' ran command ', command,  '.');
             return;
         }
         ee.emit('default', client);
-        // console.log('command', command);
     });
-     // console.log(client.nickname);
 });
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 });
-
